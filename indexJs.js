83cc26ba35cc063f92988cecceec3970031190de
Nadia Jamhari <62807626+nadiajamhari@users.noreply.github.com>
2022-08-15 22:50:24 +0800

    responsive mobile view

--- indexJs.js
+++ indexJs.js
@@ -1,11 +1,23 @@
 $(document).ready(function () {
   $("#submitButton").click(function () {
-    console.log("LOL");
     var email = $("#email").val();
     if (email === "") {
       document.getElementById("errorImage").style.visibility = "visible";
       var errorMessage = "The field is required.";
       document.getElementById("errorMessage").innerHTML = errorMessage;
+    } else {
+      let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+      var result = regex.test(email);
+      if (!result) {
+        document.getElementById("errorImage").style.visibility = "visible";
+        var errorMessage = "Please provide a valid email";
+        document.getElementById("errorMessage").innerHTML = errorMessage;
+      }
     }
+  });
+
+  $("#email").keyup(function () {
+    document.getElementById("errorImage").style.visibility = "hidden";
+    document.getElementById("errorMessage").innerHTML = "";
   });
 });
